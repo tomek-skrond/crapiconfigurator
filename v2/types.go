8ade98a8a731811b15f636b675e01a4aa8b8fd94
tomek-skrond <tomek2.skrond@gmail.com>
2024-06-17 04:09:28 +0200

    refactor struct

--- v2/types.go
+++ v2/types.go
@@ -3,13 +3,13 @@ package crapiconfigurator
 type Config struct {
 	GlobalConfig
 	TargetURL string `json:"target_url"`
-	LoginURL  string `json:"login_url"`
 }
 
 type GlobalConfig struct {
 	Hostname string `json:"hostname" yaml:"hostname"`
 	Email    string `json:"email" yaml:"email"`
 	Password string `json:"password" yaml:"password"`
+	LoginURL string `json:"login_url" yaml:"login_url"`
 }
 
 type TokenResponse struct {
